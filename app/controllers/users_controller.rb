f5c0e063bfde61cf3632cabef8cea4a93ed3ed2a
flahertyj <flahertyj@gmail.com>
2016-11-20 20:36:34 -0700

    Default to array if no books_read for user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,6 @@ class UsersController < ApplicationController
   end
 
   def get_books
-    render json: {books_read: @current_user.books_read}
+    render json: {books_read: @current_user.books_read ? @current_user.books_read.present? : []}
   end
 end
